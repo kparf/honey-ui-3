3cf548f428ff9c65b67e23b45f5e8ddc1679ace9
Kiryl Parfiankou <kiryl.parfiankou@itechart-group.com>
2020-04-24 14:16:43 +0300

    runner was updated

--- runner.js
+++ runner.js
@@ -1,5 +1,8 @@
 const http = require('http');
+const { fork } = require('child_process');
 const httpProxy = require('http-proxy');
 
+
+fork(`${__dirname}/node_modules/.bin/pm2`, ['start', `${__dirname}/node_modules/.bin/next`, `--`, `start`]);
 
 httpProxy.createProxyServer({target:'http://localhost:3000'}).listen();
